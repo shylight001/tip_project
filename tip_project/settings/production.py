286ca66bba2e6688358dd7620649c40fb1e893d1
unknown <Kevin Ou>
2018-07-26 01:35:35 -0700

    fix production BASED_DIR

--- tip_project/settings/production.py
+++ tip_project/settings/production.py
@@ -17,7 +17,7 @@ except ImportError:
     print('Unable to import base settings file')
     
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production

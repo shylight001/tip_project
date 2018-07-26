084f707d7830c2f9cfb2ca9188f59823095f93cb
unknown <Kevin Ou>
2018-07-26 01:01:57 -0700

    changed error in prooduction setting.py

--- tip_project/settings/production.py
+++ tip_project/settings/production.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pure-woodland-86484.herokuapp.com']
 
 
 # Application definition
@@ -79,12 +79,13 @@ WSGI_APPLICATION = 'tip_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 import dj_database_url
+
 DATABASES = {
     # 'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
-    'default': dj_database_url.config{}
+    'default': dj_database_url.config()
     # {
         # 'ENGINE': 'django.db.backends.postgresql',
         # 'NAME': 'tips',

cf168e481d4f2b9a3b3dfe91dba9f054184db039
unknown <Kevin Ou>
2018-07-26 01:42:40 -0700

    clean production setting

--- tip_project/settings/production.py
+++ tip_project/settings/production.py
@@ -81,19 +81,7 @@ WSGI_APPLICATION = 'tip_project.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': dj_database_url.config()
-    # {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'tips',
-        # 'USER': os.environ['DB_USER'],
-        # 'PASSWORD': os.environ['DB_PASSWORD'],
-        # 'HOST': 'localhost',
-        # 'PORT': '5432',
-    # }
 }
 
 

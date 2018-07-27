dc55b058efb4357138abe69845b8c67ade76b1d9
unknown <Kevin Ou>
2018-07-27 14:04:34 -0700

    use other way to see logging

--- tip_project/settings/production.py
+++ tip_project/settings/production.py
@@ -136,50 +136,50 @@ STATICFILES_DIRS = (
 )
 
 # Logging to fix server 500
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'mysite.log',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers':['file'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'MYAPP': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-        },
-    }
-}
 # LOGGING = {
     # 'version': 1,
     # 'disable_existing_loggers': False,
+    # 'formatters': {
+        # 'verbose': {
+            # 'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            # 'datefmt' : "%d/%b/%Y %H:%M:%S"
+        # },
+        # 'simple': {
+            # 'format': '%(levelname)s %(message)s'
+        # },
+    # },
     # 'handlers': {
-        # 'console': {
-            # 'class': 'logging.StreamHandler',
+        # 'file': {
+            # 'level': 'DEBUG',
+            # 'class': 'logging.FileHandler',
+            # 'filename': 'mysite.log',
+            # 'formatter': 'verbose'
         # },
     # },
     # 'loggers': {
         # 'django': {
-            # 'handlers': ['console'],
-             # 'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+            # 'handlers':['file'],
+            # 'propagate': True,
+            # 'level':'DEBUG',
         # },
-    # },
+        # 'MYAPP': {
+            # 'handlers': ['file'],
+            # 'level': 'DEBUG',
+        # },
+    # }
 # }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
